0bc9e5fd26dd2e1d642519017140976f921a681c
ChristopherTeyWeixian <64050082+ChristopherTeyWeixian@users.noreply.github.com>
2020-04-22 21:19:49 +0800

    Update Question 2
    Finish qn 2

--- Question2.java
+++ Question2.java
@@ -9,8 +9,16 @@ public class Question2
      * 
      * Hint: in.nextDouble() to read double
      */
-     
-    Scanner in = new Scanner(System.in);
+    Scanner in= new Scanner(System.in);
+    System.out.println("Enter Weight in kg:"); 
+    double weight= in.nextDouble(); 
+    
+    System.out.println("Enter Height in metres:"); 
     double height = in.nextDouble();
+    double bmi=weight/(height*height);
+
+    System.out.println("Height is "+height);
+    System.out.println("Weight is "+weight);
+    System.out.println("BMI is "+bmi);
   }
 }
