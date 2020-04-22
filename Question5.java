5011ef8c27f0a32b2cd5171a159386f89b040111
ChristopherTeyWeixian <64050082+ChristopherTeyWeixian@users.noreply.github.com>
2020-04-22 23:37:01 +0800

    Update Qn5
    Finish Question 5

--- Question5.java
+++ Question5.java
@@ -25,8 +25,47 @@ public class Question5
      *     2
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
+     Scanner in = new Scanner(System.in);
      
-    Scanner in = new Scanner(System.in);
-    
+     //prompt input
+     System.out.print("Enter the number of integers:");
+     int numberinput= in.nextInt();
+     int[]numberlist= new int[numberinput];
+     
+     //prompt for the previous inputted numbers of integers
+     for(int i=0; i<numberinput ;i++)
+     {
+       System.out.print("Enter number "+(i+1)+" : ");
+       numberlist[i]=Integer.parseInt(in.next());
+     }
+
+    int number=0;
+    int HighestModeCount=0;
+    for(int b=0;b<numberlist.length;b++)
+    //the number to count for occurences
+    {
+      int count=0;//Reset count
+      for(int c=0;c<numberlist.length;c++)
+      {
+        if(numberlist[b]==numberlist[c])//count if same numbers
+        {
+          count++;
+        }
+      }
+      if (count==HighestModeCount && numberlist[b]>number)
+      //Take higher number if there is two number that have the same occurences for this question
+      {
+        number=numberlist[b];
+       
+      }
+      //Overwrite if occurences of the number is higher than the old number
+      else if(count>HighestModeCount)
+      {
+        number=numberlist[b];
+        HighestModeCount=count;
+      }
+    }
+    //display
+  System.out.print("Mode is "+number);
   }
 }
