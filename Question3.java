efa497380d9aca34d756d459d44be2a073c7a742
ChristopherTeyWeixian <64050082+ChristopherTeyWeixian@users.noreply.github.com>
2020-04-22 21:23:36 +0800

    Update question 3
    Finished Question 3

--- Question3.java
+++ Question3.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Question3
 {
-  public static void main(String[] args)
+ public static void main(String[] args)
   {
     /**
      * Reads integer entered by the user, multiplies the integer by itself and print out the result.
@@ -11,6 +11,13 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.println("Enter integer to multiply by itself:"); 
+    
+    double number= in.nextInt(); 
+    double result=number*number; 
+    /*calculation*/
+      
+    System.out.print("Result is "+result);
     
   }
 }
